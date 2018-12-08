97d110e71d865d9684fd5a7cf55df36c679442f6
Sam Phippen <samphippen@googlemail.com>
2018-12-08 11:01:01 -0500

    testing script

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -ex
 
 for file in `ls fixtures/*_expected.rb`
 do
-    ruby --disable=gems run.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
+    time ruby --disable=gems run.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
     diff /tmp/out.rb $file
     if [[ $? -ne 0 ]]
     then
