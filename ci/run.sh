8e643bdbd8e31abb2f2a52f893e82a4bf1434a0a
Penelope Phippen <penelopedotzone@gmail.com>
2018-12-14 13:52:56 +0000

    Remove manual CI from test sh, github does this for us

--- ci/run.sh
+++ ci/run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 set -ex
-
-bundle exec ruby ci/set_check_status.rb "pending"
 ./test.sh
-RESULT=$?
-
-if [[ $RESULT == 0 ]]
-then
-    bundle exec ruby ci/set_check_status.rb "success"
-else
-    bundle exec ruby ci/set_check_status.rb "failure"
-fi
