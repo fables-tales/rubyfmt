594384cb99ac5563a2e619e176c4fe3017d9331f
Sam Phippen <samphippen@googlemail.com>
2018-12-16 13:25:05 +0000

    Support for %w arrays and begin rescue ensure

---
+++ fixtures/begin_ensure_rescue_actual.rb
@@ -0,0 +1,26 @@
+def func
+rescue Bees => e
+  a
+end
+
+def func2
+  begin
+  rescue Bees
+    a
+  end
+end
+
+def func3
+ensure
+  a
+end
+
+
+def func4
+  begin
+  rescue Bees
+    a
+  ensure
+    b
+  end
+end
