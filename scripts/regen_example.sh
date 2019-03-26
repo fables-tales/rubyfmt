4a4ba533996f2e9978d8ed1435ff9ebc9f405571
Teo Ljungberg <teo@teoljungberg.com>
2019-03-26 18:40:24 +0100

    Lint `.sh` files over `shellcheck(1)`
    This commit fixes the following shellcheck warnings:
    
    https://shellcheck.com/wiki/SC1009
    https://shellcheck.com/wiki/SC1020
    https://shellcheck.com/wiki/SC1072
    https://shellcheck.com/wiki/SC1073
    https://shellcheck.com/wiki/SC2001
    https://shellcheck.com/wiki/SC2006
    https://shellcheck.com/wiki/SC2044
    https://shellcheck.com/wiki/SC2045
    https://shellcheck.com/wiki/SC2046
    https://shellcheck.com/wiki/SC2053
    https://shellcheck.com/wiki/SC2086
    https://shellcheck.com/wiki/SC2094
    https://shellcheck.com/wiki/SC2162
    https://shellcheck.com/wiki/SC2181
    https://shellcheck.com/wiki/SC2230

--- scripts/regen_example.sh
+++ scripts/regen_example.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 set -ex
-EXPECTED_FILENAME=`echo $1 | sed s/_actual/_expected/`
-bundle exec ruby src/rubyfmt.rb $1 $EXPECTED_FILENAME > $EXPECTED_FILENAME
-git diff $EXPECTED_FILENAME
+
+temp=$(mktemp -q)
+EXPECTED_FILENAME="${1//_actual_/_expected}"
+
+bundle exec ruby src/rubyfmt.rb "$1" "$EXPECTED_FILENAME" > "$temp" && mv "$temp" "$EXPECTED_FILENAME"
+git diff "$EXPECTED_FILENAME"
 echo "do you want to commit [Y/n]"
-read -n1 ans
+read -rn1 ans
 if [ "$ans" == "y" ]
 then
-  git add $EXPECTED_FILENAME
+  git add "$EXPECTED_FILENAME"
 else
-  git checkout $EXPECTED_FILENAME
+  git checkout "$EXPECTED_FILENAME"
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,11 +2,12 @@
 set -ex
 
 test_folder() {
-    for file in `ls $1/*_expected.rb`
+    find "$1" -name "*_expected.rb" -type f | while read -r file
     do
-        time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
-        diff -u /tmp/out.rb $file
-        if [[ $? -ne 0 ]]
+        # shellcheck disable=SC2001
+        time ruby --disable=gems src/rubyfmt.rb "$(echo "$file" | sed s/expected/actual/)" > /tmp/out.rb
+
+        if ! diff -u /tmp/out.rb "$file"
         then
             echo "got diff"
             exit 1
@@ -15,17 +16,17 @@ test_folder() {
 }
 
 f_md5() {
-    if [[ -z `which md5sum` ]]
+    if command -v md5sum >/dev/null
     then
-        md5
-    else
         md5sum
+    else
+        md5
     fi
 }
 
-STRING_LITERALS_EXPECTED=`ruby string_literals_stress_test.rb | f_md5`
-STRING_LITERALS_ACTUAL=`ruby --disable=gems src/rubyfmt.rb string_literals_stress_test.rb | ruby | f_md5`
-if [[ $STRING_LITERALS_EXPECTED != $STRING_LITERALS_ACTUAL ]]
+STRING_LITERALS_EXPECTED=$(ruby string_literals_stress_test.rb | f_md5)
+STRING_LITERALS_ACTUAL=$(ruby --disable=gems src/rubyfmt.rb string_literals_stress_test.rb | ruby | f_md5)
+if [[ "$STRING_LITERALS_EXPECTED" != "$STRING_LITERALS_ACTUAL" ]]
 then
     echo "string literals are broken"
     exit 1
@@ -34,13 +35,13 @@ fi
 test_folder fixtures/
 
 RUBY_VERSION=$(ruby -v | grep -o "[0-9].[0-9]" | head -n 1)
-echo $RUBY_VERSION
+echo "$RUBY_VERSION"
 
-for dir in $(find fixtures -type d -name '2.*')
+find fixtures -type d -name '2.*' | while read -r dir
 do
     fixture_version=$(basename "$dir")
     if [[ $(echo "$fixture_version<=$RUBY_VERSION" | bc -l) -ne 0 ]]
     then
-        test_folder $dir
+        test_folder "$dir"
     fi
 done
