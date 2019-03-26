46e27fd380664f50db10958898f861a1a3c54c98
Teo Ljungberg <teo@teoljungberg.com>
2019-03-26 18:22:15 +0100

    Use `git diff --no-index` over `diff(1)`
    It gives contributors a more familiar interface with the diffs the test
    produce.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,10 +5,5 @@ cp -r fixtures/2.5 fixtures/2.6
 for file in `ls fixtures/*_expected.rb` `ls fixtures/$(ruby -v | grep -o '\d\.\d')/*_expected.rb`
 do
     time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
-    diff /tmp/out.rb $file
-    if [[ $? -ne 0 ]]
-    then
-        echo "got diff"
-        exit 1
-    fi
+    git diff --no-index /tmp/out.rb $file
 done
