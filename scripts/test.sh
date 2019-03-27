761ac628107786900eea948e894d825e94a4500d
Penelope Phippen <penelopedotzone@gmail.com>
2019-03-27 15:22:37 -0400

    Let the test script understand incrmenetal ruby versions

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 set -ex
 
-cp -r fixtures/2.5/ fixtures/2.6
-for file in `ls fixtures/*_expected.rb` `ls fixtures/$(ruby -v | grep -o '\d\.\d')/*_expected.rb`
-do
-    time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
-    diff -u /tmp/out.rb $file
-    if [[ $? -ne 0 ]]
-    then
-        echo "got diff"
-        exit 1
-    fi
-done
+test_folder() {
+    for file in `ls $1/*_expected.rb`
+    do
+        time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
+        diff /tmp/out.rb $file
+        if [[ $? -ne 0 ]]
+        then
+            echo "got diff"
+            exit 1
+        fi
+    done
+}
+
+test_folder fixtures/
+
+RUBY_VERSION=$(ruby -v | grep -o '\d\.\d')
+
+if [[ `echo "2.5<=$RUBY_VERSION" | bc -l` -ne 0 ]]
+then
+    test_folder fixtures/2.5
+fi
+
+if [[ `echo "2.6<=$RUBY_VERSION" | bc -l` -ne 0 ]]
+then
+    test_folder fixtures/2.6
+fi
