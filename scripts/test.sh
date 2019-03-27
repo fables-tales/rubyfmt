cd4845fd9de83a18407359f40d105f98f5653cd2
Teo Ljungberg <teo@teoljungberg.com>
2019-03-27 15:02:41 +0100

    Use `diff -u`

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@ cp -r fixtures/2.5 fixtures/2.6
 for file in `ls fixtures/*_expected.rb` `ls fixtures/$(ruby -v | grep -o '\d\.\d')/*_expected.rb`
 do
     time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
-    git diff --no-index /tmp/out.rb $file
+    diff -u /tmp/out.rb $file
 done
