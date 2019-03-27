8e777ce4ca76564b8b00585957a40f20283d828a
Teo Ljungberg <teo@teoljungberg.com>
2019-03-27 15:03:42 +0100

    Re-add short circuit

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,9 @@ for file in `ls fixtures/*_expected.rb` `ls fixtures/$(ruby -v | grep -o '\d\.\d
 do
     time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
     diff -u /tmp/out.rb $file
+    if [[ $? -ne 0 ]]
+    then
+        echo "got diff"
+        exit 1
+    fi
 done
