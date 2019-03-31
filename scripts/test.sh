d483b361dd6e13b5708ce0b04eeef1eadc790b83
Teo Ljungberg <teo@teoljungberg.com>
2019-03-31 16:55:39 +0200

    Find using maxdepth

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 set -ex
 
 test_folder() {
-    find "$1" -name "*_expected.rb" -type f | while read -r file
+    find "$1" -name "*_expected.rb" -maxdepth 1 | while read -r file
     do
         # shellcheck disable=SC2001
         time ruby --disable=gems src/rubyfmt.rb "$(echo "$file" | sed s/expected/actual/)" > /tmp/out.rb
@@ -37,7 +37,7 @@ test_folder fixtures/
 RUBY_VERSION=$(ruby -v | grep -o "[0-9].[0-9]" | head -n 1)
 echo "$RUBY_VERSION"
 
-find fixtures -type d -name '2.*' | while read -r dir
+find fixtures/ -type d -name "2.*" | while read -r dir
 do
     fixture_version=$(basename "$dir")
     if [[ $(echo "$fixture_version<=$RUBY_VERSION" | bc -l) -ne 0 ]]
