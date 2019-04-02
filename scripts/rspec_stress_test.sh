f93c03e413e51a2bc8701322e075395100f74efc
Sam Phippen <samphippen@googlemail.com>
2019-04-02 11:55:03 -0400

    Does this work?

--- scripts/rspec_stress_test.sh
+++ scripts/rspec_stress_test.sh
@@ -1,47 +1,53 @@
 #!/bin/bash
 set -ex
 
-mkdir -p /tmp/rubyfmt
+run_rspec() {
+    (
+    cd ./workspace/rubyfmt/rspec-core || exit
+    bundle exec rspec
+    git reset --hard
+    )
+}
+
+mkdir -p ./workspace/rubyfmt
 if [ -z "${GITHUB_REF+x}" ]
 then
     echo "not on github"
 else
-    rm -rf /tmp/rubyfmt/rspec-core
+    rm -rf ./workspace/rubyfmt/rspec-core
 fi
-ls /tmp/rubyfmt/rspec-core/lib || git clone --depth=1 https://github.com/rspec/rspec-core /tmp/rubyfmt/rspec-core
+ls ./workspace/rubyfmt/rspec-core/lib || git clone --depth=1 https://github.com/rspec/rspec-core ./workspace/rubyfmt/rspec-core
 
 (
-cd /tmp/rubyfmt/rspec-core || exit
+cd ./workspace/rubyfmt/rspec-core || exit
 git reset --hard
 bundle
 )
 
-FILES=$(find /tmp/rubyfmt/rspec-core/lib -type f | grep -i '\.rb$')
+FILES=$(find ./workspace/rubyfmt/rspec-core/lib -type f | grep -i '\.rb$')
 for FN in $FILES
 do
     echo "running rubyfmt on $FN"
-    ruby --disable=gems src/rubyfmt.rb "$FN" > /tmp/rubyfmt/this_one.rb
-    ruby --disable=gems src/rubyfmt.rb /tmp/rubyfmt/this_one.rb > "$FN"
+    ruby --disable=gems src/rubyfmt.rb "$FN" > ./workspace/rubyfmt/this_one.rb
+    ruby --disable=gems src/rubyfmt.rb ./workspace/rubyfmt/this_one.rb > "$FN"
+
+    if [[ "$1" == "--debug" ]]
+    then
+        run_rspec
+    fi
 done
-(
-cd /tmp/rubyfmt/rspec-core || exit
-bundle exec rspec
-git reset --hard
-)
+
+run_rspec
 
 # refmt.rb replaces rubyfmt.rb
-ruby --disable=gems src/rubyfmt.rb src/rubyfmt.rb > /tmp/rubyfmt/refmt.rb
+ruby --disable=gems src/rubyfmt.rb src/rubyfmt.rb > ./workspace/rubyfmt/refmt.rb
 
-FILES=$(find /tmp/rubyfmt/rspec-core/lib -type f | grep -i '\.rb$')
+FILES=$(find ./workspace/rubyfmt/rspec-core/lib -type f | grep -i '\.rb$')
 for FN in $FILES
 do
     echo "running rubyfmt on $FN"
-    ruby --disable=gems /tmp/rubyfmt/refmt.rb "$FN" > //tmp/rubyfmt/this_one.rb
-    ruby --disable=gems /tmp/rubyfmt/refmt.rb //tmp/rubyfmt/this_one.rb > "$FN"
+    ruby --disable=gems ./workspace/rubyfmt/refmt.rb "$FN" > /./workspace/rubyfmt/this_one.rb
+    ruby --disable=gems ./workspace/rubyfmt/refmt.rb /./workspace/rubyfmt/this_one.rb > "$FN"
 done
 
-(
-cd /tmp/rubyfmt/rspec-core || exit
-bundle exec rspec
-git reset --hard
-)
+run_rspec
