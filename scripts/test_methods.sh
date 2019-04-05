c6bed0f7fde7ec48fbfd256356799872fbbc2105
Sam Phippen <samphippen@googlemail.com>
2019-04-05 14:51:40 -0400

    fix variable names in test script

--- scripts/test_methods.sh
+++ scripts/test_methods.sh
@@ -3,9 +3,9 @@ set -ex
 
 source ./scripts/functions.sh
 
-STRING_LITERALS_EXPECTED=$(ruby methods_stress_test.rb | f_md5)
-STRING_LITERALS_ACTUAL=$(ruby --disable=gems src/rubyfmt.rb methods_stress_test.rb | ruby | f_md5)
-if [[ "$STRING_LITERALS_EXPECTED" != "$STRING_LITERALS_ACTUAL" ]]
+METHODS_EXPECTED=$(ruby methods_stress_test.rb | f_md5)
+METHODS_ACTUAL=$(ruby --disable=gems src/rubyfmt.rb methods_stress_test.rb | ruby | f_md5)
+if [[ "$METHODS_EXPECTED" != "$METHODS_ACTUAL" ]]
 then
     echo "string literals are broken"
     exit 1
