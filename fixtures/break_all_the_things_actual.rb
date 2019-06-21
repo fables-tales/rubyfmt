edc451d5c94f3383f59e46775e02f2134e4023f7
Sam Phippen <samphippen@googlemail.com>
2019-06-21 13:10:35 -0400

    block local params are hilarious

---
+++ fixtures/break_all_the_things_actual.rb
@@ -0,0 +1,23 @@
+def foo(
+  aqwoeifjqwoiefjqwoifjoqiwfjoiqwefj,
+  cqwefqwefqwjeofijqwoeifjqwoiefjqwoiefjoiqwejfoqiwejfoi=nil
+)
+end
+
+def foo(
+  a,
+  b=nil
+)
+end
+
+{:a => :b}
+
+a do |;x|
+
+end
+
+a do |;qwioefijqwoiefjoqiwefjoqwiejfqwe,qwoiefjqowiejfoiqwejfoiqwjfoiqwejfiowqejfqwieqwoeifjoqwiejfiowqefjoijf|
+
+end
+
+a = [fqwoijfqwoiefjqwioefjqw, qwefoijqwefoiweq, qwefqwefewq, fqewfqwef, qwefqwefewq, qwefqwef, qewfweq]
