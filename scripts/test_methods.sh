dfa2371e63a3f4dfef3bef6fdf6306015c616942
Penelope Phippen <penelopedotzone@gmail.com>
2019-07-14 08:53:38 -0400

    more toplevel cleanup

--- scripts/test_methods.sh
+++ scripts/test_methods.sh
@@ -3,10 +3,10 @@ set -ex
 
 source ./scripts/functions.sh
 
-METHODS_EXPECTED=$(ruby methods_stress_test.rb | f_md5)
-METHODS_ACTUAL=$(ruby --disable=gems build/rubyfmt.rb methods_stress_test.rb | ruby | f_md5)
+METHODS_EXPECTED=$(ruby ci/methods_stress_test.rb | f_md5)
+METHODS_ACTUAL=$(ruby --disable=gems build/rubyfmt.rb ci/methods_stress_test.rb | ruby | f_md5)
 if [[ "$METHODS_EXPECTED" != "$METHODS_ACTUAL" ]]
 then
-    echo "string literals are broken"
+    echo "methods literals are broken"
     exit 1
 fi
