f5b1cdd0ee4632c63878df19c203b3c3136ce344
Sam Phippen <samphippen@googlemail.com>
2019-07-14 08:53:38 -0400

    more toplevel cleanup

--- scripts/test_string_literals.sh
+++ scripts/test_string_literals.sh
@@ -3,8 +3,8 @@ set -ex
 
 source ./scripts/functions.sh
 
-STRING_LITERALS_EXPECTED=$(ruby string_literals_stress_test.rb | f_md5)
-STRING_LITERALS_ACTUAL=$(ruby --disable=gems build/rubyfmt.rb string_literals_stress_test.rb | ruby | f_md5)
+STRING_LITERALS_EXPECTED=$(ruby ci/string_literals_stress_test.rb | f_md5)
+STRING_LITERALS_ACTUAL=$(ruby --disable=gems build/rubyfmt.rb ci/string_literals_stress_test.rb | ruby | f_md5)
 if [[ "$STRING_LITERALS_EXPECTED" != "$STRING_LITERALS_ACTUAL" ]]
 then
     echo "string literals are broken"
