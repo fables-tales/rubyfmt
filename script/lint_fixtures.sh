1a0b3a3e8a970f81ece7c22d7cbabf36dfc21d37
Corey Alexander <coreyja@gmail.com>
2020-03-07 13:14:29 -0500

    Make sure all fixture files are named correctly

--- script/lint_fixtures.sh
+++ script/lint_fixtures.sh
@@ -5,14 +5,14 @@ lint_dir_deep() {
     current_dir="$1"
 
     find "$current_dir" -name "*.rb" | while read -r file; do
-      actual_file="${file//expected/actual}"
-      expected_file="${file//actual/expected}"
+      actual_file="${file//_expected.rb/_actual.rb}"
+      expected_file="${file//_actual.rb/_expected.rb}"
 
+      [[ "$actual_file" == "$expected_file" ]] &&
+        echo "$file does not match the naming conventions for the fixtures. The file must end in '_actual.rb' or '_expected.rb'" && exit 1
       [ -f "$actual_file" ] || (echo "$file exists but $actual_file does not" && exit 1)
       [ -f "$expected_file" ] || (echo "$file exists but $expected_file does not" && exit 1)
     done
 }
-
-RUBY_VERSION=$(ruby -v | grep -o "[0-9].[0-9]" | head -n 1)
 
 lint_dir_deep "fixtures"
