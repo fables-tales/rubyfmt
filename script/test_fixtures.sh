ea46813d9a11265a065f55c0b7bced248fda6751
Corey Alexander <coreyja@gmail.com>
2020-03-07 13:00:47 -0500

    Change script to work with this file structure, basically by going back closer to the orig version

--- script/test_fixtures.sh
+++ script/test_fixtures.sh
@@ -4,14 +4,12 @@ set -e
 test_folder() {
     current_dir="$1"
 
-    ## Check that both files or neither exist
-    if [ -f "$current_dir/expected.rb" ] || [ -f "$current_dir/actual.rb" ]; then
-      [ -f "$current_dir/expected.rb" ] || (echo "$current_dir is missing the expected.rb file" && exit 1)
-      [ -f "$current_dir/actual.rb" ] || (echo "$current_dir is missing the actual.rb file" && exit 1)
+    find "$current_dir" -name "*_expected.rb" -maxdepth 1 | while read -r expected_file; do
+      actual_file="${expected_file//expected/actual}"
 
       ## Test if the formatting works as expected
-      time ruby --disable=gems rubyfmt.rb "$current_dir/actual.rb" > /tmp/out.rb
-      if ! diff -u /tmp/out.rb "$current_dir/expected.rb" > /tmp/diff.out
+      time ruby --disable=gems rubyfmt.rb "$actual_file" > /tmp/out.rb
+      if ! diff -u /tmp/out.rb "$expected_file" > /tmp/diff.out
       then
         echo "got diff between formated formatted actual and expected"
         cat /tmp/diff.out
@@ -19,17 +17,17 @@ test_folder() {
       fi
 
       ## Test if the formatting is idempotent
-      time ruby --disable=gems rubyfmt.rb "$current_dir/expected.rb" > /tmp/out.rb
-      if ! diff -u /tmp/out.rb "$current_dir/expected.rb" > /tmp/diff.out
+      time ruby --disable=gems rubyfmt.rb "$expected_file" > /tmp/out.rb
+      if ! diff -u /tmp/out.rb "$expected_file" > /tmp/diff.out
       then
         echo "got diff between formatted expected and expected (not idempotent)"
         cat /tmp/diff.out
         exit 1
       fi
-    fi
+    done
 
     ## Recurse over ruby version dirs
-    find "$current_dir" -type d -mindepth 1 -maxdepth 1 -name 'ruby-' | while read -r dir
+    find "$current_dir" -type d -mindepth 1 -maxdepth 1 -name '2.' | while read -r dir
     do
         base="$(basename "$dir")"
         fixture_version=${base#"ruby-"}
@@ -38,14 +36,7 @@ test_folder() {
             test_folder "$dir"
         fi
     done
-
-    # Recurse over the other dirs
-    find "$current_dir" -type d -mindepth 1 -maxdepth 1 -not -name 'ruby-' | while read -r dir
-    do
-          test_folder "$dir"
-    done
 }
-
 
 RUBY_VERSION=$(ruby -v | grep -o "[0-9].[0-9]" | head -n 1)
 
