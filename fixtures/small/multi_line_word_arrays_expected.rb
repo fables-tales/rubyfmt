07dfbbc26dd6f91f8ebb2b775bc2963aa2dea8e0
Penelope Phippen <penelopedotzone@gmail.com>
2020-03-20 10:35:23 -0400

    various fixes

--- fixtures/small/multi_line_word_arrays_expected.rb
+++ fixtures/small/multi_line_word_arrays_expected.rb
@@ -1,7 +1,27 @@
-a = %w[a b c d]
-b = %W[a#{"string_literal"} b]
-c = %i[a b c]
-d = %I[a b c]
+a = %w[
+a
+b
+c
+d
+]
+
+b = %W[
+a#{"string_literal"}
+b
+]
+
+c = %i[
+a
+b
+c
+]
+
+d = %I[
+a
+b
+c
+]
+
 p(a)
 p(b)
 p(c)
