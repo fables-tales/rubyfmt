d41f6fa466759f42ab58833f71f866104b907434
Wade Tandy <wtandy@bloomberg.net>
2020-06-07 17:11:40 -0400

    Add script for generating a shim rubyfmt script
    The README currently recommends setting up a `rubyfmt` alias as part of
    the installation process, but depending on how you are invoking the
    command, not every environment will work with an alias.  This script
    will install a shim script at the location of the user's choice:
    
    ``` bash
    $ ./script/install_shim.sh ~/bin/rubyfmt
    ```
    
    The script adds the `RUBYFMT_USE_RELEASE` variable as well so as to
    prevent unnecessary debug output.

---
+++ script/install_shim.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+if [ $# -eq 0 ]
+  then
+    echo "Please pass a file location to install the shim script"
+    exit 1
+fi
+
+echo "#!/usr/bin/env bash
+export RUBYFMT_USE_RELEASE=1
+$(which ruby) --disable=all $(pwd)/rubyfmt.rb \$@" > "$1"
+chmod +x "$1"
