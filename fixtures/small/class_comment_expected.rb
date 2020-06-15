8a71216d4a282d0e7f8f53eb28984b392a7376cb
Wade Tandy <wade.tandy@gmail.com>
2020-06-15 00:35:42 -0400

    Failing test for multiline comment in empty method
    This test demonstrates a formatting error when there are multiline
    comments inside an empty method. When formatting, the first line of the
    comment remains in the method, while all other lines get pulled out to
    just after the method. Additionally, if the method is the last method in
    the file, only the second line gets pulled after `end`. Everything else
    in the comment, as well as a comment following the method, gets dropped
    entirely.

--- fixtures/small/class_comment_expected.rb
+++ fixtures/small/class_comment_expected.rb
@@ -11,9 +11,26 @@ class Bees
   # this one gets a proceeding newline because it's prefixed by a statement
   class Bar
   end
+  # this one gets a proceeding newline because it's prefixed by a statement
 
   def bees
     # no proceeding newline here
     a
   end
+
+  def empty_method
+    # comment inside empty method
+    # second line of comment
+    # third line of comment
+    # fourth line of comment
+  end
+  # comment after empty method
+
+  def empty_method_end_of_file
+    # comment inside empty method
+    # second line of comment
+    # third line of comment
+    # fourth line of comment
+  end
+  # comment after empty method
 end
