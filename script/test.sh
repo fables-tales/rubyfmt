9852bdda5635b5c78687b406d90541611da9ce10
Penelope Phippen <penelopedotzone@gmail.com>
2020-07-02 13:21:38 -0400

    i hate this

--- script/test.sh
+++ script/test.sh
@@ -5,6 +5,7 @@ make
 make release
 
 export RUBYFMT_USE_RELEASE=1
+uname -a
 ./script/tests/test_string_literals.sh
 #./script/tests/test_methods.sh
 #./script/tests/test_fixtures.sh
--- script/tests/test_string_literals.sh
+++ script/tests/test_string_literals.sh
@@ -3,9 +3,11 @@ set -ex
 
 source ./script/functions.sh
 
+sysctl -w kernel.core_pattern="$(pwd)/core.%u.%e.%p"
 STRING_LITERALS_EXPECTED=$(ruby ci/string_literals_stress_test.rb | f_md5)
+echo 'a' | f_rubyfmt
 f_rubyfmt ci/string_literals_stress_test.rb || echo 'command failed'
-find .
+ls -lat
 exit 1
 STRING_LITERALS_ACTUAL=$(f_rubyfmt ci/string_literals_stress_test.rb | ruby | f_md5)
 if [[ "$STRING_LITERALS_EXPECTED" != "$STRING_LITERALS_ACTUAL" ]]
