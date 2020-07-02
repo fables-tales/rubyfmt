0fb2c053607b566674ac18fe47455c919ebdab1e
Penelope Phippen <penelopedotzone@gmail.com>
2020-07-02 12:22:00 -0400

    attempt to fix

--- script/tests/test_string_literals.sh
+++ script/tests/test_string_literals.sh
@@ -4,6 +4,9 @@ set -ex
 source ./script/functions.sh
 
 STRING_LITERALS_EXPECTED=$(ruby ci/string_literals_stress_test.rb | f_md5)
+f_rubyfmt ci/string_literals_stress_test.rb 
+find .
+exit 1
 STRING_LITERALS_ACTUAL=$(f_rubyfmt ci/string_literals_stress_test.rb | ruby | f_md5)
 if [[ "$STRING_LITERALS_EXPECTED" != "$STRING_LITERALS_ACTUAL" ]]
 then
