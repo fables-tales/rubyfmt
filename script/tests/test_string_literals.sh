d511111f7cbaa976a3c0994196479bf2014f3590
Penelope Phippen <penelopedotzone@gmail.com>
2020-07-02 17:54:59 -0400

    string lit

--- script/tests/test_string_literals.sh
+++ script/tests/test_string_literals.sh
@@ -4,10 +4,6 @@ set -ex
 source ./script/functions.sh
 
 STRING_LITERALS_EXPECTED=$(ruby ci/string_literals_stress_test.rb | f_md5)
-echo 'a' | f_rubyfmt 2>file 1>file2
-f_rubyfmt ci/string_literals_stress_test.rb || echo 'command failed'
-ls -lat
-exit 1
 STRING_LITERALS_ACTUAL=$(f_rubyfmt ci/string_literals_stress_test.rb | ruby | f_md5)
 if [[ "$STRING_LITERALS_EXPECTED" != "$STRING_LITERALS_ACTUAL" ]]
 then
