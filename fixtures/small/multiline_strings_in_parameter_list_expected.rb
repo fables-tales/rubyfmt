ea23dfe732ebe01bf6d44cbf776936691ca3eabd
Pete Higgins <pete@peterhiggins.org>
2021-01-19 22:19:36 -0800

    Fix extra newlines added into parameter list with multiline strings.

---
+++ fixtures/small/multiline_strings_in_parameter_list_expected.rb
@@ -0,0 +1,18 @@
+puts(
+  "
+    yellow
+    fruit
+    peel
+    cheese
+    noodle
+    sauce",
+  :burrito,
+  # add some comments here,
+  # why not
+  "
+    jasmine
+    earl grey
+    oolong
+  ",
+  :rice
+)
