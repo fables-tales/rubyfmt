e7762dfee5002a008b2b2392180ddb0cdd24f714
Reese Williams <reese@stripe.com>
2022-11-22 05:53:21 -0800

    Fixtures

---
+++ fixtures/small/visibility_modifier_expected.rb
@@ -0,0 +1,24 @@
+class Foo
+  private def bar
+    42
+  end
+
+  private def baz(a, b, c)
+    a + b + c
+  end
+end
+
+module WhiteTeaBowl
+  sig do
+    params(walking_this: Path)
+  end
+  module_function def i_choose_one
+  end
+
+  sig do
+    void
+  end
+  patch_of def sunlight
+    "after another"
+  end
+end
