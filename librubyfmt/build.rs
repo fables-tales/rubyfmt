bbecd3c3fc4f0c4921164cada15656d4f13708d7
Reese Williams <reese@reesew.io>
2022-11-28 10:17:36 -0600

    Fix build for MacOS Big Sur ARM builds (#371)

--- librubyfmt/build.rs
+++ librubyfmt/build.rs
@@ -52,6 +52,7 @@ fn main() -> Output {
         .file("src/rubyfmt.c")
         .object(ruby_checkout_path.join(&ripper))
         .include(ruby_checkout_path.join("include"))
+        .include(ruby_checkout_path.join(".ext/include/arm64-darwin20"))
         .include(ruby_checkout_path.join(".ext/include/arm64-darwin21"))
         .include(ruby_checkout_path.join(".ext/include/arm64-darwin22"))
         .include(ruby_checkout_path.join(".ext/include/x86_64-darwin21"))
