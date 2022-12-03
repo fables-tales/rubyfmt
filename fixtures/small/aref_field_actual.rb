9fdd0b993c007c8fa03a3d257fd5b9d1c908bc2e
Reese Williams <reese@reesew.io>
2022-12-03 09:28:25 -0800

    Add fixtures for non-assign arefs

--- fixtures/small/aref_field_actual.rb
+++ fixtures/small/aref_field_actual.rb
@@ -11,3 +11,13 @@ a[begin; ""; rescue StandardException; ""; end] = ""
 a[] = ""
 # Users can override #[]= to have multiple args
 a[1, 2] = ""
+
+a[puts a]
+a[puts(a)]
+a[()]
+a[class A; def foo; puts a; end; end]
+a[begin; ""; rescue StandardException; ""; end]
+# Users can override #[] to not have args
+a[]
+# Users can override #[] to have multiple args
+a[1, 2]
--- fixtures/small/aref_field_expected.rb
+++ fixtures/small/aref_field_expected.rb
@@ -20,3 +20,22 @@ a[begin
 a[] = ""
 # Users can override #[]= to have multiple args
 a[1, 2] = ""
+
+a[puts(a)]
+a[puts(a)]
+a[()]
+
+a[class A
+    def foo
+      puts(a)
+    end
+end]
+a[begin
+    ""
+  rescue StandardException
+    ""
+  end]
+# Users can override #[] to not have args
+a[]
+# Users can override #[] to have multiple args
+a[1, 2]
