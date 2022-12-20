8004d1063a5750e4b525bb8571c693eab5471d15
Reese Williams <reese@reesew.io>
2022-12-20 13:00:41 -0700

    Fixtures

---
+++ fixtures/small/breakables_over_line_length_actual.rb
@@ -0,0 +1,7 @@
+# Nested breakables over the max line length
+# (Sung to the tune of "Carol of the Bells")
+[ReallyLongThing, ReallyLongThing, ReallyLongThing, ReallyLongThing, [ReallyLongThing, ReallyLongThing, ReallyLongThing, ReallyLongThing, ReallyLongThing, ReallyLongThing, ReallyLongThing], ReallyLongThing, ReallyLongThing]
+
+if Opus::Foo::Bar::Baz::ReallyLongName::Example::ExampleExampleExampleExampleExampleExampleExampleExample.calls_a_thing(a, b)
+    puts ""
+end
