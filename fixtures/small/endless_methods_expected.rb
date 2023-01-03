b52393a87295df7d174d0c300510a2a2b134a6da
Reese Williams <reese@reesew.io>
2023-01-03 09:24:18 +0000

    Fixtures

---
+++ fixtures/small/endless_methods_expected.rb
@@ -0,0 +1,13 @@
+class ForWeiMu
+  def we_are_both(travelers) = "dark-eyed with love".uppercase
+  def self.and_both_possessed_of = white_cloud_mind
+
+  private def why_set_out_for = EastMountain.when_here!
+  def spring_grasses = grow_deeper_day.by_day?
+end
+
+def more_examples = begin
+  dangerous_stuff!
+rescue
+  puts("saved the day")
+end
