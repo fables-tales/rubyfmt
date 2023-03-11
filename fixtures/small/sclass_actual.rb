bf811945a0130e92bbf92ee5541df63d95d492e4
Reese Williams <reese@reesew.io>
2023-03-11 20:42:37 +0000

    Fixtures

--- fixtures/small/sclass_actual.rb
+++ fixtures/small/sclass_actual.rb
@@ -3,4 +3,17 @@ class Foo
   end
 end
 
+# The moon beams white on the peach blossoms,
+# the Milky Way moored silvering at midnight.
+class MoonBeam
+  # I wonder if the nightingale has noticed
+  # the spring spirit already alive in the branch?
+  class << self
+    # I can hardly get to sleep
+    # as if tenderness were a sickness.
+    def show
+    end
+  end
+end
+
 Foo.machine
