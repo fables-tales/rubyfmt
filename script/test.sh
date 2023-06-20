8e31845fe70cfd6f445fbd55d4e2feaceefbcff8
Nathan Froyd <froydnj@gmail.com>
2023-06-20 15:33:30 -0400

    add aarch64 cross-compilation bits to CI and release (#436)

--- script/test.sh
+++ script/test.sh
@@ -4,7 +4,31 @@ set -euxo pipefail
 rm -rf tmp/
 source ./script/functions.sh
 
-cargo build --release
+target="${TARGET:-native}"
+
+case "$target" in
+  native)
+    cargo build --release
+    ;;
+  aarch64-unknown-linux-gnu)
+    sudo apt-get update
+    sudo apt-get install -y gcc-aarch64-linux-gnu libc6-dev-arm64-cross
+
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
+      CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
+      TARGET_CC=aarch64-linux-gnu-gcc \
+      TARGET_AR=aarch64-linux-gnu-ar \
+      cargo build --release --target aarch64-unknown-linux-gnu
+
+    # Don't try to test things: even with QEMU support, it would take a long time.
+    exit 0
+    ;;
+  *)
+    echo "Unknown cross-compilation target $target"
+    exit 1
+    ;;
+esac
+
 
 export RUBYFMT_USE_RELEASE=1
 uname -a
