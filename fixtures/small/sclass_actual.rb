862652c2a65b96af873025fc0bc08d4a43fe57c7
Reese Williams <reese@reesew.io>
2023-07-21 14:34:28 -0700

    Revert "Fix comment rendering for sclasses (#420)" (#441)
    * Revert "Fix comment rendering for sclasses (#420)"
    
    This reverts commit 3b06f3c2594571cdfc5f605be69ad74f33fa2637.
    
    * Add shellcheck-ignore
    
    * Add a test case

--- fixtures/small/sclass_actual.rb
+++ fixtures/small/sclass_actual.rb
@@ -3,17 +3,11 @@ class Foo
   end
 end
 
-# The moon beams white on the peach blossoms,
-# the Milky Way moored silvering at midnight.
-class MoonBeam
-  # I wonder if the nightingale has noticed
-  # the spring spirit already alive in the branch?
+class Bar
   class << self
-    # I can hardly get to sleep
-    # as if tenderness were a sickness.
-    def show
-    end
   end
+
+  def another_method!; end
 end
 
 Foo.machine
