b4c201b2b456395ce4a57b049b2099555261130e
Reese Williams <reese@reesew.io>
2024-01-26 16:59:27 +0000

    comment

--- librubyfmt/build.rs
+++ librubyfmt/build.rs
@@ -46,7 +46,6 @@ fn main() -> Output {
     let new_checkout_sha = get_ruby_checkout_sha();
 
     // Only rerun this build if the ruby_checkout has changed
-    // boop
     match old_checkout_sha {
         Some(old_sha)
             if old_sha == new_checkout_sha && env::var("FORCE_FULL_RUBY_BUILD").is_err() => {}
