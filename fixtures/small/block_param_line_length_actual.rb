ada3c39c75157be264329ed1ccf8b2cff533c6f5
Reese Williams <reese@reesew.io>
2024-02-21 05:55:21 +0000

    Fixtures

---
+++ fixtures/small/block_param_line_length_actual.rb
@@ -0,0 +1,7 @@
+ThisIsAReallyLongClassName::WithSomeModulesInsideItThatHaveAMethodThatWeWillCallRightAroundHereeeee.do_stuff(boom) do |foo|
+    some_stuff!
+end
+
+ThisIsAReallyLongClassName::ButSlightShorterWithMoreCalls.foo.bar.baz.quux.what_comes_after_quux_idk_aaaahhhh.map { |model|
+    body_of_the_call
+}
