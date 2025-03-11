06f1c8e86d9516b29bcf458231dd6b53f56ef820
Reese Williams <reese@reesew.com>
2025-03-11 18:37:29 +0900

    Remove `librubyfmt` caching from CI to fix ubuntu build (#479)
    * Maybe caching is just busted
    
    * Just rip out the old caching mechanism

--- librubyfmt/build.rs
+++ librubyfmt/build.rs
@@ -47,8 +47,7 @@ fn main() -> Output {
 
     // Only rerun this build if the ruby_checkout has changed
     match old_checkout_sha {
-        Some(old_sha)
-            if old_sha == new_checkout_sha && env::var("FORCE_FULL_RUBY_BUILD").is_err() => {}
+        Some(old_sha) if old_sha == new_checkout_sha => {}
         _ => {
             make_configure(&ruby_checkout_path)?;
             run_configure(&ruby_checkout_path)?;
