f1b7f36b27c8980e14599d72642b3a9234f5fff3
Reese Williams <reese@reesew.io>
2025-03-26 18:26:25 +0900

    Convert stress tests into Rust-based tests

--- script/test_prism.sh
+++ script/test_prism.sh
@@ -10,9 +10,7 @@ cargo build --release
 uname -a
 
 # TODO(reese): uncomment these as we build out Prism support
-# ./script/tests/test_string_literals.sh
-# ./script/tests/test_array_literals.sh
-# ./script/tests/test_methods.sh
+# cargo test --test stress_test
 # ./script/tests/test_cli_interface.sh
 # ./script/tests/test_c_main.sh
 # cargo test --test error_handling_test
