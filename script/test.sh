356cabe47615512e9beca358c3bc0d56f0ec9f4e
Reese Williams <reese@reesew.io>
2025-04-05 18:13:47 +0900

    Use github hosted ARM runners and remove cross-compilation infra

--- script/test.sh
+++ script/test.sh
@@ -4,31 +4,5 @@ set -euxo pipefail
 rm -rf tmp/
 source ./script/functions.sh
 
-target="${TARGET:-native}"
-
-case "$target" in
-  native)
-    cargo build --release
-    ;;
-  aarch64-unknown-linux-gnu)
-    sudo apt-get update
-    sudo apt-get install -y gcc-aarch64-linux-gnu libc6-dev-arm64-cross
-
-    env BINDGEN_EXTRA_CLANG_ARGS_aarch64-unknown-linux-gnu="--sysroot=/usr/aarch64-linux-gnu" \
-      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
-      CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
-      TARGET_CC=aarch64-linux-gnu-gcc \
-      TARGET_AR=aarch64-linux-gnu-ar \
-      cargo build --release --target aarch64-unknown-linux-gnu
-
-    # Don't try to test things: even with QEMU support, it would take a long time.
-    exit 0
-    ;;
-  *)
-    echo "Unknown cross-compilation target $target"
-    exit 1
-    ;;
-esac
-
 uname -a
 cargo test --release
