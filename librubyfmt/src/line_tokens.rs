896ec7f1ba46f7175170f996219edac042de29db
Reese Williams <reese@reesew.io>
2025-04-21 16:10:28 -0500

    Make is_single_line_breakable_garbage only care about whitespace

--- librubyfmt/src/line_tokens.rs
+++ librubyfmt/src/line_tokens.rs
@@ -159,7 +159,6 @@ impl ConcreteLineToken {
     pub fn is_single_line_breakable_garbage(&self) -> bool {
         match self {
             Self::DirectPart { part } => part == &"".to_string(),
-            Self::Comma => true,
             Self::Space => true,
             _ => false,
         }
