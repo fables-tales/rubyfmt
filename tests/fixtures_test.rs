4cfa2045ee2069fc1e0db81e9e640d4b089d72b4
Reese Williams <reese@reesew.io>
2025-12-15 11:45:03 +0700

    [prism] Properly enforce hash rocket usage rules

--- tests/fixtures_test.rs
+++ tests/fixtures_test.rs
@@ -49,7 +49,6 @@ const DISABLED_PRISM_TESTS: &[&'static str] = &[
     "small_dyna_symbol_with_escapes",
     "small_empty_arg_paren",
     "small_gemfile",
-    "small_hash_rocket_usage",
     "small_inline_rescue",
     "small_lambda",
     "small_method_annotation",
