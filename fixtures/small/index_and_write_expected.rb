2d7aa762c907c3309466e2b98338c1a52c2b9475
Reese Williams <reese@reesew.com>
2025-12-19 10:51:59 -0600

    [prism] Implement index write nodes (#642)
    * [prism] Implement index write nodes
    
    * fmt

---
+++ fixtures/small/index_and_write_expected.rb
@@ -0,0 +1,5 @@
+foo[bar] &&= value
+foo[0] &&= 1
+hash[:key] &&= "new_value"
+array[i + 1] &&= compute_value
+obj.data[key] &&= fallback
---
+++ fixtures/small/index_operator_write_actual.rb
@@ -0,0 +1,7 @@
+foo[bar] += value
+foo[0] -= 1
+hash[:key] *= 2
+array[i + 1] /= divisor
+obj.data[key] += increment
+counter[name] |= flag
+bits[index] &= mask
