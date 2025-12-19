56cec999d663d1adc665dbec2ace2c8f152dae8b
Reese Williams <reese@reesew.com>
2025-12-19 08:11:10 -0600

    [prism] Fix indentation inside arefs at the start of call chains (#633)
    * [prism] Fix indentation inside arefs at the start of call chains
    
    * Use call_node we already read

--- tests/fixtures_test.rs
+++ tests/fixtures_test.rs
@@ -19,7 +19,6 @@ const DISABLED_RIPPER_TESTS: &[&'static str] = &[
 
 // These tests will have their prism variants automatically ignored
 const DISABLED_PRISM_TESTS: &[&'static str] = &[
-    "small_aref_in_call",
     "small_brace_blocks_with_no_args",
     "small_dyna_symbol_with_escapes",
     "small_percent_q",
