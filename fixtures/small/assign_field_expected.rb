72adc63e69fa38683259e09bc7180815cb496df5
Reese Williams <reese@reesew.com>
2025-12-19 20:57:16 -0600

    [prism] Don't wrap attr_write calls in breakables (#659)

--- fixtures/small/assign_field_expected.rb
+++ fixtures/small/assign_field_expected.rb
@@ -1,3 +1,7 @@
 foo.bar = :c
 foo.bar.baz = :c
 foo.Bar = :c
+
+T.unsafe(foo).bar = "Some really long value where I have to type so much just to get it all the way to"
+  .call(120, characters)
+  .all_the_way_to_here
