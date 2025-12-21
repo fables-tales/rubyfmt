5b2998586e60816b769f47ed49ddecd070b5fd5a
Reese Williams <reese@reesew.com>
2025-12-21 16:23:16 -0600

    [prism] Implement `MatchPredicateNode` and `MatchRequiredNode` (#685)
    * [prism] Implement MatchPredicateNode and MatchRequiredNode
    
    * fmt

---
+++ fixtures/small/prism/match_predicate_actual.rb
@@ -0,0 +1,11 @@
+1 in x
+result in value
+
+foo   in   bar
+
+if 1 in x
+  puts x
+end
+
+[1, 2, 3].first in num
+some_method(arg) in result
---
+++ fixtures/small/prism/match_required_expected.rb
@@ -0,0 +1,7 @@
+1 => x
+result => value
+
+foo => bar
+
+[1, 2, 3].first => num
+some_method(arg) => result
