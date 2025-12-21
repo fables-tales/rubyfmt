155a697607a85cc91e273fad389dc78d0c61c9b8
Reese Williams <reese@reesew.com>
2025-12-21 15:10:26 -0600

    [prism] Support embdoc (=begin/=end) comments (#683)

---
+++ fixtures/small/prism/embdoc_actual.rb
@@ -0,0 +1,43 @@
+def haiku
+  # Violets -
+    how_precious
+    on_a_mountain_path
+
+=begin
+    Early autumn -
+    rice field, ocean,
+    one green.
+=end
+    by_basho!
+end
+
+=begin
+  The thief left it behind:
+  the moon
+  at my window.
+=end
+
+class Poet
+    def self.recite
+        poem {
+            recite!
+=begin
+    From the firefly
+    in my hands,
+    cold light
+=end
+            recite! while more_poetry?
+        }
+    end
+end
+
+def
+=begin
+=end
+a; end
+
+:\
+=begin
+not the symbol name
+=end
+symbol_name
---
+++ fixtures/small/prism/embdoc_expected.rb
@@ -0,0 +1,42 @@
+def haiku
+  # Violets -
+  how_precious
+  on_a_mountain_path
+
+=begin
+    Early autumn -
+    rice field, ocean,
+    one green.
+=end
+  by_basho!
+end
+
+=begin
+  The thief left it behind:
+  the moon
+  at my window.
+=end
+
+class Poet
+  def self.recite
+    poem {
+      recite!
+=begin
+    From the firefly
+    in my hands,
+    cold light
+=end
+      recite! while more_poetry?
+    }
+  end
+end
+
+=begin
+=end
+def a
+end
+
+=begin
+not the symbol name
+=end
+:symbol_name
