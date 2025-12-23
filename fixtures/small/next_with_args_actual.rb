8fbc6c12e15c579668e3d06d5ec664a02278481d
Reese Williams <reese@reesew.com>
2025-12-23 12:12:10 -0600

    [prism] Never use parens with `next` (#714)

--- fixtures/small/next_with_args_actual.rb
+++ fixtures/small/next_with_args_actual.rb
@@ -1,3 +1,13 @@
 [].each do ||
 next 1
+next Foo.new(
+    a,
+    b
+)
+next(
+    Foo.new(
+        a,
+        b
+    )
+)
 end
--- fixtures/small/next_with_args_expected.rb
+++ fixtures/small/next_with_args_expected.rb
@@ -1,3 +1,11 @@
 [].each do
   next 1
+  next Foo.new(
+    a,
+    b
+  )
+  next (Foo.new(
+    a,
+    b
+  ))
 end
