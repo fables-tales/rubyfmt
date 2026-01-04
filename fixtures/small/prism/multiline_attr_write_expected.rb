75f4cdd8aaa7c0390707b6e96f56cc82ba2c76c4
Reese Williams <reese@reesew.io>
2026-01-04 19:08:16 +0100

    Fixture

---
+++ fixtures/small/prism/multiline_attr_write_expected.rb
@@ -0,0 +1,13 @@
+v2_request
+  .additional_data_private
+  .sub_elements
+  .specific_collect_pointer = Constant::Thing
+
+v2_request.additional_data_private.sub_elements.specific_collect_pointer.lets_make_this_quite_a_bit_longer = Constant::Thing
+
+v2_request
+  .additional_data_private
+  .sub_elements
+  .specific_collect_pointer
+  .lets_make_this_quite_a_bit_longer
+  .over_120_chars_now = Constant::Thing
