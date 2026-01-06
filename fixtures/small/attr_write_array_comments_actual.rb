26472e6fdea7d17fe619ec707b11c142a5fc0cf4
Reese Williams <reese@reesew.com>
2026-01-06 16:29:29 +0100

    [prism] Don't prematurely wind comments in attr_writes (#767)

---
+++ fixtures/small/attr_write_array_comments_actual.rb
@@ -0,0 +1,17 @@
+a.items = [
+  # First item comment
+  1,
+  # Second item comment
+  2
+]
+
+x.list = [
+  "value"
+  # trailing
+]
+
+foo.bar.values = [
+  # inside array
+  :a,
+  :b
+]
---
+++ fixtures/small/attr_write_hash_comments_actual.rb
@@ -0,0 +1,32 @@
+a.bees = {
+  # Stuff
+  honey: "hive"
+}
+
+x.config = {
+  # First setting
+  debug: true,
+  # Second setting
+  verbose: false
+}
+
+y.options = {
+  enabled: true
+  # trailing comment
+}
+
+obj.data = {
+  # Comment line 1
+  # Comment line 2
+  key: "value"
+}
+
+foo.bar.baz = {
+  # inside hash
+  item: 123
+}
+
+maybe&.setting = {
+  # safe navigation comment
+  value: nil
+}
