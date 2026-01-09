6c9d235f70bc6e1d4cc1ab55823540adb64db557
Reese Williams <reese@reesew.io>
2026-01-09 17:06:40 +0100

    [prism] Check for whitespaces escsapes at front of heredoc lines

---
+++ fixtures/small/heredoc_nonuniform_escape_indent_actual.rb
@@ -0,0 +1,6 @@
+<<~EOS
+    \tMore indented line with escape
+  Less indented line
+    \sAnother more indented with space escape
+  Back to less indent
+EOS
---
+++ fixtures/small/heredoc_space_escape_indent_expected.rb
@@ -0,0 +1,5 @@
+<<~EOS
+  \sLeading space escape
+  \s\sDouble space escape
+  \t\sMixed tab and space escape
+EOS
