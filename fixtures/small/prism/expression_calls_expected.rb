21d27d715810a08c4fd03eb1d535d500e9cb0305
Reese Williams <reese@reesew.io>
2026-01-15 19:41:03 +0100

    [prism] Add tests around comments after call chains with leading expressions

---
+++ fixtures/small/prism/expression_calls_expected.rb
@@ -0,0 +1,48 @@
+[
+  CommonFields::OBVIOUSLY,
+  CommonFields::THESE,
+  CommonFields::ARE,
+  CommonFields::FAKE_RESOURCE.with_wombo(true).with_combo(true).with_explosion(true)
+].map do |field|
+  # Swap things out, to make sure.
+  override || field
+end
+
+{
+  having: "Having a Coke With You",
+  with: you
+}.freeze
+
+{
+  having: "Having a Coke With You",
+  with: you
+  # by Mark Leidner
+}.freeze
+
+{
+  having: "Having a Coke With You",
+  with: you
+  # by Mark Leidner
+}.freeze
+
+{
+  having: "Having a Coke With You",
+  with: you
+}
+  # by Mark Leidner
+  .freeze
+
+{
+  something: "wicked"
+}.this_way {
+  comes
+  # spookyyyy
+}
+
+{musical: "wicked"}
+  # The original broadway cast recording!
+  .sing("Defying Gravity")
+
+# TODO(@reese): Fix this failing test case
+# {musical: "wicked"}
+#   .sing("Defying Gravity") # The original broadway cast recording!
