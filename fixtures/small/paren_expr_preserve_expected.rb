b907d7be26b6fabb0ae7d0701f2f8205ea959815
Penelope Phippen <penelope@hey.com>
2026-01-16 10:35:27 -0500

    bees

--- fixtures/small/paren_expr_preserve_expected.rb
+++ fixtures/small/paren_expr_preserve_expected.rb
@@ -26,24 +26,3 @@ check((condition and action))
 # `or` keyword (low precedence) - `foo (a or b)` must become `foo((a or b))`
 foo((a or b))
 provide((default or fallback))
-
-# case expressions
-foo(
-  (case x
-  when 1
-    :one
-  when 2
-    :two
-  else
-    :other
-  end)
-)
-
-# begin/end blocks
-foo(
-  (begin
-    risky_operation
-  rescue
-    fallback
-  end)
-)
