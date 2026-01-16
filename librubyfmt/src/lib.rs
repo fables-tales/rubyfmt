a365a1c8caa0dd5bf20e13947cb3b92cf02d94a6
Reese Williams <reese@reesew.io>
2026-01-16 16:05:44 +0100

    More ruby/crate bits

--- librubyfmt/src/lib.rs
+++ librubyfmt/src/lib.rs
@@ -1,9 +1,7 @@
 #![deny(warnings, missing_copy_implementations)]
 #![allow(clippy::upper_case_acronyms, clippy::enum_variant_names)]
 
-use serde::de::value;
 use std::io::{Cursor, Write};
-use std::slice;
 use std::str;
 
 #[cfg(all(feature = "use_jemalloc", not(target_env = "msvc")))]
@@ -12,13 +10,9 @@ static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 pub type RawStatus = i64;
 
-#[macro_use]
-pub mod ruby;
 mod comment_block;
-mod de;
 mod delimiters;
 mod file_comments;
-mod format;
 mod format_prism;
 mod heredoc_string;
 mod intermediary;
@@ -27,38 +21,20 @@ mod line_tokens;
 mod parser_state;
 mod render_queue_writer;
 mod render_targets;
-mod ripper_tree_types;
-mod ruby_ops;
 mod string_escape;
 mod types;
 mod util;
 
 use file_comments::FileComments;
 use parser_state::ParserState;
-use ruby_ops::{ParseError, Parser, RipperTree, load_rubyfmt};
 
 #[cfg(debug_assertions)]
 use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
 
-unsafe extern "C" {
-    pub fn Init_ripper();
-    pub fn rb_gc_disable();
-}
-
-pub struct RubyfmtString(Box<str>);
-
-#[derive(Debug, Copy, Clone)]
-pub enum InitStatus {
-    OK = 0,
-    ERROR = 1,
-}
-
 #[derive(Debug)]
 pub enum RichFormatError {
     SyntaxError,
-    RipperParseFailure(value::Error),
     IOError(std::io::Error),
-    OtherRubyError(String),
 }
 
 impl RichFormatError {
@@ -69,9 +45,7 @@ impl RichFormatError {
     fn as_format_error(&self) -> FormatError {
         match self {
             RichFormatError::SyntaxError => FormatError::SyntaxError,
-            RichFormatError::RipperParseFailure(_) => FormatError::RipperParseFailure,
             RichFormatError::IOError(_) => FormatError::IOError,
-            RichFormatError::OtherRubyError(_) => FormatError::OtherRubyError,
         }
     }
 }
@@ -87,149 +61,26 @@ pub enum FormatError {
     DiffDetected = 5,
 }
 
-pub fn format_buffer(buf: &str, use_prism: bool) -> Result<String, RichFormatError> {
+pub fn format_buffer(buf: &str) -> Result<String, RichFormatError> {
     let out_data = vec![];
     let mut output = Cursor::new(out_data);
 
-    if use_prism {
-        let parse_result = ruby_prism::parse(buf.as_bytes());
-        if parse_result.errors().next().is_some() {
-            return Err(RichFormatError::SyntaxError);
-        }
-        let end_data = parse_result.data_loc();
-
-        toplevel_format_program_with_prism(
-            &mut output,
-            parse_result.node(),
-            parse_result.comments(),
-            buf.as_bytes(),
-            end_data,
-        )?;
-    } else {
-        let (tree, file_comments, end_data) = run_parser_on(buf)?;
-        toplevel_format_program(&mut output, tree, file_comments, end_data)?;
+    let parse_result = ruby_prism::parse(buf.as_bytes());
+    if parse_result.errors().next().is_some() {
+        return Err(RichFormatError::SyntaxError);
     }
+    let end_data = parse_result.data_loc();
+
+    toplevel_format_program_with_prism(
+        &mut output,
+        parse_result.node(),
+        parse_result.comments(),
+        buf.as_bytes(),
+        end_data,
+    )?;
+
     output.flush().expect("flushing to a vec should never fail");
     Ok(String::from_utf8(output.into_inner()).expect("we never write invalid UTF-8"))
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn rubyfmt_init() -> libc::c_int {
-    init_logger();
-    let res = ruby_ops::setup_ruby();
-    if res.is_err() {
-        return InitStatus::ERROR as libc::c_int;
-    }
-
-    let res = unsafe { load_ripper() };
-    if res.is_err() {
-        return InitStatus::ERROR as libc::c_int;
-    }
-
-    let res = unsafe { load_rubyfmt() };
-    if res.is_err() {
-        return InitStatus::ERROR as libc::c_int;
-    }
-
-    InitStatus::OK as libc::c_int
-}
-
-/// # Safety
-/// this function will fail, very badly, if len specifies more bytes than is
-/// available in the passed buffer pointer. It will also fail if the passed
-/// data isn't utf8.
-/// Please don't pass non-utf8 too small buffers.
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn rubyfmt_format_buffer(
-    ptr: *const u8,
-    len: usize,
-    err: *mut i64,
-) -> *mut RubyfmtString {
-    let input = unsafe { str::from_utf8_unchecked(slice::from_raw_parts(ptr, len)) };
-    let output = format_buffer(input, false);
-    match output {
-        Ok(o) => {
-            unsafe { *err = FormatError::OK as i64 };
-            Box::into_raw(Box::new(RubyfmtString(o.into_boxed_str())))
-        }
-        Err(e) => {
-            unsafe { *err = e.as_format_error() as i64 };
-            std::ptr::null::<RubyfmtString>() as _
-        }
-    }
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn rubyfmt_string_ptr(s: &RubyfmtString) -> *const u8 {
-    s.0.as_ptr()
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn rubyfmt_string_len(s: &RubyfmtString) -> usize {
-    s.0.len()
-}
-
-#[unsafe(no_mangle)]
-extern "C" fn rubyfmt_string_free(rubyfmt_string: *mut RubyfmtString) {
-    unsafe {
-        drop(Box::from_raw(rubyfmt_string));
-    }
-}
-
-// Safety: This function expects a functioning Ruby VM
-unsafe fn load_ripper() -> Result<(), ()> {
-    unsafe {
-        // trick ruby in to thinking ripper is already loaded
-        ruby::eval_str(
-            r#"
-    $LOADED_FEATURES << "ripper.bundle"
-    $LOADED_FEATURES << "ripper.so"
-    $LOADED_FEATURES << "ripper.rb"
-    $LOADED_FEATURES << "ripper/core.rb"
-    $LOADED_FEATURES << "ripper/sexp.rb"
-    $LOADED_FEATURES << "ripper/filter.rb"
-    $LOADED_FEATURES << "ripper/lexer.rb"
-    "#,
-        )?;
-
-        // init the ripper C module
-        Init_ripper();
-
-        //load each ripper program
-        ruby::eval_str(include_str!("../ruby_checkout/ext/ripper/lib/ripper.rb"))?;
-        ruby::eval_str(include_str!(
-            "../ruby_checkout/ext/ripper/lib/ripper/core.rb"
-        ))?;
-        ruby::eval_str(include_str!(
-            "../ruby_checkout/ext/ripper/lib/ripper/lexer.rb"
-        ))?;
-        ruby::eval_str(include_str!(
-            "../ruby_checkout/ext/ripper/lib/ripper/filter.rb"
-        ))?;
-        ruby::eval_str(include_str!(
-            "../ruby_checkout/ext/ripper/lib/ripper/sexp.rb"
-        ))?;
-
-        rb_gc_disable();
-    }
-    Ok(())
-}
-
-pub fn toplevel_format_program<W: Write>(
-    writer: &mut W,
-    tree: RipperTree,
-    file_comments: FileComments,
-    end_data: Option<&str>,
-) -> Result<(), RichFormatError> {
-    let mut ps = ParserState::new(file_comments);
-    let v: ripper_tree_types::Program =
-        de::from_value(tree).map_err(RichFormatError::RipperParseFailure)?;
-
-    format::format_program(&mut ps, v, end_data);
-
-    ps.write(writer).map_err(RichFormatError::IOError)?;
-    writer.flush().map_err(RichFormatError::IOError)?;
-    Ok(())
 }
 
 pub fn toplevel_format_program_with_prism<W: Write>(
@@ -247,13 +98,6 @@ pub fn toplevel_format_program_with_prism<W: Write>(
     ps.write(writer).map_err(RichFormatError::IOError)?;
     writer.flush().map_err(RichFormatError::IOError)?;
     Ok(())
-}
-
-fn run_parser_on(buf: &str) -> Result<(RipperTree, FileComments, Option<&str>), RichFormatError> {
-    Parser::new(buf).parse().map_err(|e| match e {
-        ParseError::SyntaxError => RichFormatError::SyntaxError,
-        ParseError::OtherRubyError(s) => RichFormatError::OtherRubyError(s),
-    })
 }
 
 pub fn init_logger() {
