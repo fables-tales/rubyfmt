fdaf7d2559c558f24d34b17a372014542c8cdf30
Nathan Froyd <froydnj@gmail.com>
2026-01-17 16:33:25 -0500

    avoid `write!` for very basic output (#794)

--- librubyfmt/src/render_queue_writer.rs
+++ librubyfmt/src/render_queue_writer.rs
@@ -310,7 +310,7 @@ impl<'src> RenderQueueWriter<'src> {
 
         for line_token in tokens.into_iter() {
             let s = line_token.into_ruby();
-            write!(writer, "{}", s)?
+            writer.write_all(s.as_bytes())?;
         }
         Ok(())
     }
