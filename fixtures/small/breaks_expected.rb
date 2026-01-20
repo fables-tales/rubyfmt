df8444ca94f6d60daa4c53d96c99577254fdd146
Reese Williams <reese@reesew.io>
2026-01-20 17:57:01 +0000

    Add some multi-line and more complex array examples

--- fixtures/small/breaks_expected.rb
+++ fixtures/small/breaks_expected.rb
@@ -7,4 +7,17 @@ loop do
   break Some::Very::Long::Thing::That::Will::Extend::Over::Multiple::Lines::And::Never::Ends.await_result(
     workflow_id: workflow_id
   ), with_a_second_value
+  break [
+    off_reservation_payment,
+    payment_intent_mode,
+    Return.new(outcome: Outcome::Succeeded, attemptes: off_reservation_payment.attempts + 1)
+  ]
+  break [
+    off_reservation_payment,
+    payment_intent_mode,
+    Return.new(
+      outcome: Outcome::Succeeded,
+      attemptes: off_reservation_payment.attempts + 1
+    )
+  ]
 end
