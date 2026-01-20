1bd27368e5207dc505077704a10845eab224cb16
Reese Williams <reese@reesew.io>
2026-01-20 15:55:10 +0000

    Fix `break` multilining

--- fixtures/small/breaks_actual.rb
+++ fixtures/small/breaks_actual.rb
@@ -1,4 +1,6 @@
 loop do
   break no
   break
+  break Some::Very::Long::Thing::That::Will::Extend::Over::Multiple::Lines::And::Never::Ends.await_result(workflow_id: workflow_id)
+  break Some::Very::Long::Thing::That::Will::Extend::Over::Multiple::Lines::And::Never::Ends.await_result(workflow_id: workflow_id), with_a_second_value
 end
