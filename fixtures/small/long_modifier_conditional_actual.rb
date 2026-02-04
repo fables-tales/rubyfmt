52f4ccdcb005ef3f2ac9211595b9db8b6331a723
Reese Williams <reese@reesew.io>
2026-02-04 19:17:01 +0000

    Add testcase

--- fixtures/small/long_modifier_conditional_actual.rb
+++ fixtures/small/long_modifier_conditional_actual.rb
@@ -3,3 +3,15 @@ some_really_long_method_name_that_takes_up_space! if another_really_long_conditi
 some_really_long_method_name_that_takes_up_space! unless another_really_long_condition_that_makes_this_line_exceed_one_hundred_twenty
 
 short_method_call if a_condition_that_fits_within_the_line_length_limit_of_one_hundred_twenty
+
+module X
+  class Y
+    module More
+      def test
+        # comments make a difference?
+        amount = amount_from_api ||
+          (Opus::MonetaryFlows::Private::API::BeesItems::ParamTransformer.validation_amount(line_items) if !line_items.nil?)
+      end
+    end
+  end
+end
--- fixtures/small/long_modifier_conditional_expected.rb
+++ fixtures/small/long_modifier_conditional_expected.rb
@@ -7,3 +7,19 @@ unless another_really_long_condition_that_makes_this_line_exceed_one_hundred_twe
 end
 
 short_method_call if a_condition_that_fits_within_the_line_length_limit_of_one_hundred_twenty
+
+module X
+  class Y
+    module More
+      def test
+        # comments make a difference?
+        amount = amount_from_api ||
+          (
+            if !line_items.nil?
+              Opus::MonetaryFlows::Private::API::BeesItems::ParamTransformer.validation_amount(line_items)
+            end
+          )
+      end
+    end
+  end
+end
