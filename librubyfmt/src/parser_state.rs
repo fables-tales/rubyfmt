53d59f63f2dc92aa3280ad430c71b8caccf3c124
Reese Williams <reese@reesew.io>
2026-02-27 12:50:45 -0600

    Clean up redundant Cow usage in variables scopes

--- librubyfmt/src/parser_state.rs
+++ librubyfmt/src/parser_state.rs
@@ -67,7 +67,7 @@ pub struct ParserState<'src> {
     formatting_context: Vec<FormattingContext>,
     insert_user_newlines: bool,
     spaces_after_last_newline: ColNumber,
-    scopes: Vec<Vec<Cow<'src, str>>>,
+    scopes: Vec<Vec<&'src str>>,
     /// Whether we're currently rendering inside a squiggly heredoc's content.
     /// Used to mark nested non-squiggly heredocs so they don't get incorrect indentation.
     inside_squiggly_heredoc: bool,
@@ -79,7 +79,7 @@ impl<'src> ParserState<'src> {
             .last()
             .expect("it's never empty")
             .iter()
-            .any(|e| e == s)
+            .any(|e| *e == s)
     }
     pub(crate) fn new_scope<F>(&mut self, f: F)
     where
@@ -89,7 +89,7 @@ impl<'src> ParserState<'src> {
         f(self);
         self.scopes.pop();
     }
-    pub(crate) fn bind_variable(&mut self, s: impl Into<Cow<'src, str>>) {
+    pub(crate) fn bind_variable(&mut self, s: &'src str) {
         self.scopes
             .last_mut()
             .expect("it's never empty")
