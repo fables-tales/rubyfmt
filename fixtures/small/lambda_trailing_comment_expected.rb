486b79fe13f31adb23465938bf076a28d7db928d
Reese Williams <reese@reesew.com>
2026-03-22 16:59:43 -0500

    Fix comment placement at the end of lambda bodies (#846)
    * Fixtures
    
    * Fix
    
    * do/end fixtures

---
+++ fixtures/small/lambda_trailing_comment_expected.rb
@@ -0,0 +1,23 @@
+-> (local) {
+  cached_connections = applications.map { |x| x }
+  return cached_connections.size == applications.size
+  # for now assume cache miss
+}
+
+-> {
+  y = x + 1
+  y * 2
+  # trailing comment
+}
+
+-> (local) do
+  cached_connections = applications.map { |x| x }
+  return cached_connections.size == applications.size
+  # for now assume cache miss
+end
+
+-> do
+  y = x + 1
+  y * 2
+  # trailing comment
+end
