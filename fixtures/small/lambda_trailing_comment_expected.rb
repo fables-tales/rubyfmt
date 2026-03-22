1e5786e93f506d8b9ca71bfe5193e423ec04590e
Reese Williams <reese@reesew.io>
2026-03-22 16:40:45 -0500

    do/end fixtures

--- fixtures/small/lambda_trailing_comment_expected.rb
+++ fixtures/small/lambda_trailing_comment_expected.rb
@@ -9,3 +9,15 @@
   y * 2
   # trailing comment
 }
+
+-> (local) do
+  cached_connections = applications.map { |x| x }
+  return cached_connections.size == applications.size
+  # for now assume cache miss
+end
+
+-> do
+  y = x + 1
+  y * 2
+  # trailing comment
+end
