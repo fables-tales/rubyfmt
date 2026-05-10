d818f5275c847c5bd221d0235cf18ec158532783
Reese Williams <reese@reesew.com>
2026-05-10 15:39:49 -0500

    [fix] Properly track hard newlines in brace blocks (#875)
    * Test fixtures
    
    * [fix] Properly recurse through breakable token types when tooking for hard newlines

---
+++ fixtures/small/breakable_idempotency_expected.rb
@@ -0,0 +1,52 @@
+foo(
+  -> {
+    a
+    b
+  }
+)
+foo(
+  proc {
+    a
+    b
+  }
+)
+foo(
+  lambda {
+    a
+    b
+  }
+)
+foo(
+  bar {
+    a
+    b
+  }
+)
+[
+  proc {
+    a
+    b
+  },
+  other
+]
+{
+  key: proc {
+    a
+    b
+  }
+}
+foo(
+  [1, 2, 3],
+  proc {
+    a
+    b
+  }
+)
+nested = foo(
+  bar(
+    baz {
+      a
+      b
+    }
+  )
+)
