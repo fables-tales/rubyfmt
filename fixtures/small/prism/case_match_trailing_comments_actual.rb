3fd2134443d65e56cc08fffc8264fa4518b46f69
Reese Williams <reese@reesew.com>
2026-05-12 16:37:19 -0500

    [fix] Properly indent `case`/`in` statements (#887)
    * Add test fixtures
    
    * [fix] Properly indent case/in statements

---
+++ fixtures/small/prism/case_match_trailing_comments_actual.rb
@@ -0,0 +1,11 @@
+case 1
+in 2 # Two
+in 3 # Three
+end
+
+case value
+in :a # comment a
+  do_a
+in :b # comment b
+  do_b
+end
---
+++ fixtures/small/prism/case_match_trailing_comments_expected.rb
@@ -0,0 +1,15 @@
+case 1
+# Two
+in 2
+  # Three
+in 3
+end
+
+case value
+# comment a
+in :a
+  do_a
+  # comment b
+in :b
+  do_b
+end
