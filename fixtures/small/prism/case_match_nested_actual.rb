3c06da6b2da13d4667b7f4abd68645cd0174d0fa
Reese Williams <reese@reesew.io>
2026-05-12 15:31:15 -0500

    Add test fixtures

---
+++ fixtures/small/prism/case_match_nested_actual.rb
@@ -0,0 +1,23 @@
+module App
+  config = {db: {user: 'admin', password: 'abc123'}}
+
+  case config
+  in {db: {user:}}
+    puts "Connect with user '#{user}'"
+  in {connection: {username: }}
+    puts "Connect with user '#{username}'"
+  else
+    puts "Unrecognized structure of config"
+  end
+end
+
+class Foo
+  def bar(value)
+    case value
+    in 1
+      "one"
+    in 2
+      "two"
+    end
+  end
+end
