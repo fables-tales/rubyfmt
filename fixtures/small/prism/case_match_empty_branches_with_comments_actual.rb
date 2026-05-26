82b975006a94ad3f549503b116c767f3764536d4
Reese Williams <reese@reesew.io>
2026-05-26 15:55:26 -0500

    Fixtures

---
+++ fixtures/small/prism/case_match_empty_branches_with_comments_actual.rb
@@ -0,0 +1,18 @@
+case 1 # Case
+in 1 # In 1
+in 2 # In 2
+  # Foo
+  2
+end # End
+
+case 0 # Case
+in 1 # In 1
+in 2 # In 2
+end # End
+foo # Foo
+
+case 0 # Case
+when 1 # When 1
+when 2 # When 2
+end # End
+bar # Bar
