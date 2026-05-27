c891abd9c42bc4ac266b090b06db24991734f99e
Reese Williams <reese@reesew.com>
2026-05-27 10:26:25 -0500

    [fix] Only push start-of-file comments on empty lines (#895)
    * Fixtures
    
    * [fix] Only push start-of-file comments on empty lines
    
    * clippy

---
+++ fixtures/small/prism/case_match_empty_branches_with_comments_expected.rb
@@ -0,0 +1,32 @@
+# Case
+case 1
+# In 1
+in 1
+  # In 2
+in 2
+  # Foo
+  2
+  # End
+end
+
+# Case
+case 0
+# In 1
+in 1
+  # In 2
+in 2
+  # End
+end
+# Foo
+foo
+
+# Case
+case 0
+# When 1
+when 1
+  # When 2
+when 2
+  # End
+end
+# Bar
+bar
