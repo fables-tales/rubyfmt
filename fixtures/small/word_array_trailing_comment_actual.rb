360365a43c4145957d7b5740ad6c0f2059895f36
Reese Williams <reese@reesew.io>
2026-06-02 09:36:27 -0500

    Fixtures

---
+++ fixtures/small/word_array_trailing_comment_actual.rb
@@ -0,0 +1,21 @@
+a = %w[
+  b
+] #c
+
+d = %w[
+  e
+  f
+] # trailing
+
+g = %i[
+  h
+  i
+] # symbol trailing
+
+j = %W[
+  k
+] # cap-W trailing
+
+l = %I[
+  m
+] # cap-I trailing
---
+++ fixtures/small/word_array_trailing_comment_expected.rb
@@ -0,0 +1,26 @@
+#c
+a = %w[
+  b
+]
+
+# trailing
+d = %w[
+  e
+  f
+]
+
+# symbol trailing
+g = %i[
+  h
+  i
+]
+
+# cap-W trailing
+j = %W[
+  k
+]
+
+# cap-I trailing
+l = %I[
+  m
+]
