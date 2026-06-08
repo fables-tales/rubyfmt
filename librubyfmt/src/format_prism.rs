389fcb1e6df7f10ce6b8f5038c4101feb527e4f5
Nick Dower <nicholasdower@gmail.com>
2026-06-08 08:18:01 +0200

    Add comment

--- librubyfmt/src/format_prism.rs
+++ librubyfmt/src/format_prism.rs
@@ -4728,6 +4728,7 @@ fn format_rescue_node<'src>(ps: &mut ParserState<'src>, rescue_node: prism::Resc
     let exceptions = rescue_node.exceptions();
     let reference = rescue_node.reference();
     if !exceptions.is_empty() {
+        // Preemptively extract inline comments so they land before rescue, not inside the breakable.
         ps.at_offset(exceptions.first().unwrap().location().start_offset());
         ps.with_start_of_line(false, |ps| {
             ps.inline_breakable_of(BreakableDelims::for_binary_op(), |ps| {
