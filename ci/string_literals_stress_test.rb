3c1e75a0e21a54ccc4e08800753a1a28d99a6f86
Nick Dower <nicholasdower@gmail.com>
2026-06-12 09:32:16 +0200

    [fix] indentation & trailing whitespace in Heredoc
    An attempt at fixing a few Heredoc issues:
    * 1 tab equals 8 spaces
    * 1 space + 1 tab equals 8 spaces
    * Trailing whitespace in squiggly Heredoc should not be stripped.
    * A single whitespace-only line should be preserved
    * Multiple whitespace-only lines should be preserved
    
    See:
    https://docs.ruby-lang.org/en/master/syntax/literals_rdoc.html#here-document-literals
    
    >The indentation of the least-indented line will be removed from each
    >line of the content. Note that empty lines and lines consisting solely
    >of literal tabs and spaces will be ignored for the purposes of
    >determining indentation, but escaped tabs and spaces are considered
    >non-indentation characters.
    >
    >For the purpose of measuring an indentation, a horizontal tab is
    >regarded as a sequence of one to eight spaces such that the column
    >position corresponding to its end is a multiple of eight. The amount to
    >be removed is counted in terms of the number of spaces. If the boundary
    >appears in the middle of a tab, that tab is not removed.

--- ci/string_literals_stress_test.rb
+++ ci/string_literals_stress_test.rb
@@ -118,3 +118,75 @@ foo
 puts <<EOD.gsub("a", "b")
 "cde"
 EOD
+
+# whitespace-only lines are not considered when calculating indentation
+puts \
+<<~FOO.inspect
+  the next line contains a single space
+ 
+FOO
+
+# 1 tab equals 8 spaces, both should be considered indentation
+puts \
+<<~FOO.inspect
+        this line is indented 8 spaces
+	this line is indented 1 tab
+FOO
+
+# 1 tab plus 1 space equals 8 spaces
+puts \
+<<~FOO.inspect
+         this line is indented 9 spaces
+	 this line is indented 1 space and 1 tab
+FOO
+
+# 1 tab is greater than 7 spaces, the tab should be considered content
+puts \
+<<~FOO.inspect
+       this line is indented 7 spaces
+	this line is indented 1 tab
+FOO
+
+# trailing spaces should not be removed
+puts \
+<<-FOO.inspect
+    this line and the next have a trailing space: 
+     
+    this line and the next have a trailing tab:	
+	
+FOO
+
+# trailing spaces should not be removed (squiggly)
+puts \
+<<~FOO.inspect
+    this line and the next have a trailing space: 
+     
+    this line and the next have a trailing tab:	
+	
+FOO
+
+# single line of trailing space
+puts \
+<<-FOO.inspect
+   
+FOO
+
+# single line of trailing space (squiggly)
+puts \
+<<~FOO.inspect
+   
+FOO
+
+# multiple lines of trailing space
+puts \
+<<-FOO.inspect
+ 
+ 
+FOO
+
+# multiple lines of trailing space (squiggly)
+puts \
+<<~FOO.inspect
+ 
+ 
+FOO
